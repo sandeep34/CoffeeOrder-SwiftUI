e93350139ceb0f3cdb054f0cd132114e7dd0a7d4
Sandeep Tomar <tomarsandeep199192@gmail.com>
2023-11-01 15:42:21 +0530

    Initial Commit

---
+++ CoffeeOrderingApp/CoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingAppApp.swift
+//  CoffeeOrderingApp
+//
+//  Created by Sandeep.Tomar on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
